544e8c1d3fedb477a620d45d3b23b89addfc67ac
ArtofDrawingSwords <brandonscoolletters@hotmail.com>
2015-06-28 19:39:26 -0700

    Added basic horizontal player movement

---
+++ GameOfTheYear/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+		
+	public float maxSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		Vector3 velocity = new Vector3 (Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, 0, 0);
+		pos += velocity;
+		transform.position = pos;
+	}
+}
