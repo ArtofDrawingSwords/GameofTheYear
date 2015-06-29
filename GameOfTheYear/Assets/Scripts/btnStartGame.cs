5c76cb38538abebd271a3901288e1bd00147f839
houseofkohina <bryankhamly@gmail.com>
2015-06-29 08:14:53 -0700

    StartMenu Addition
    StartMenu Added

---
+++ GameOfTheYear/Assets/Scripts/btnStartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+namespace FMG
+{
+	public class btnStartGame : MonoBehaviour, IPointerClickHandler {
+		
+		public virtual void OnPointerClick(PointerEventData eventData)
+		{
+			Debug.Log ("Starting Game..." );
+			Application.LoadLevel("Scene1");
+		}
+	}
+}
