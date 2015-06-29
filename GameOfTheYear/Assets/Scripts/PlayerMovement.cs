31c21a95d5d580a66c0be7b40f8d4c5e144ea01e
ArtofDrawingSwords <brandonscoolletters@hotmail.com>
2015-06-28 19:56:37 -0700

    updated player movement
    added testjump and cleaned up code

--- GameOfTheYear/Assets/Scripts/PlayerMovement.cs
+++ GameOfTheYear/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 		
 	public float maxSpeed = 5f;
+	float horiVelocity;
+	float vertVelocity;
+	Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +15,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		Vector3 pos = transform.position;
-		Vector3 velocity = new Vector3 (Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, 0, 0);
+
+		//calculates velocity
+		horiVelocity = Input.GetAxis ("Horizontal") * maxSpeed * Time.deltaTime;
+		vertVelocity = Input.GetAxis ("Jump") * maxSpeed * Time.deltaTime;
+
+		//stores velocity in vector3
+		velocity = new Vector3 (horiVelocity, vertVelocity, 0);
+
+		//calculates new position and stores in Vector3
 		pos += velocity;
+
+		//update position of player
 		transform.position = pos;
+
 	}
 }
