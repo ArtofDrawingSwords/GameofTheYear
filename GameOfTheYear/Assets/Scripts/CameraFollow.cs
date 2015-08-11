99ea592d57076b335043ed76b93c6ba965db4eed
houseofkohina <bryankhamly@gmail.com>
2015-08-10 22:16:52 -0700

    CameraFollow Update
    Added a following camera. Use public variables to customize camera.

--- GameOfTheYear/Assets/Scripts/CameraFollow.cs
+++ GameOfTheYear/Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour {
-
-	public GameObject target;
-	Vector3 tempPosition;
+public class CameraFollow : MonoBehaviour 
+{
+	public float xbeforemove = 1f;	
+	public float ybeforemove = 1f;		
+	public float xcameraspeed = 6f;		
+	public float ycameraspeed = 6f;		
+	public Vector2 maximumXY;		
+	public Vector2 minimumXY;		
+	private Transform spineboy;		
 	
-	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+	{
+		spineboy = GameObject.FindGameObjectWithTag("spineboy").transform;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		tempPosition = target.transform.position;
-		tempPosition.z = transform.position.z;
-		transform.position = tempPosition;
+	bool playermaxmoveX()
+	{
+		return Mathf.Abs(transform.position.x - spineboy.position.x) > xbeforemove;
 	}
+	bool playermaxmoveY()
+	{
+		return Mathf.Abs(transform.position.y - spineboy.position.y) > ybeforemove;
+	}
+	void FixedUpdate ()
+	{
+		followme();
+	}
+	void followme ()
+	{
+		float playerx = transform.position.x;
+		float playery = transform.position.y;
+			if(playermaxmoveX())
+			playerx = Mathf.Lerp(transform.position.x, spineboy.position.x, xcameraspeed * Time.deltaTime);
+			if(playermaxmoveY())
+			playery = Mathf.Lerp(transform.position.y, spineboy.position.y, ycameraspeed * Time.deltaTime);
+				playerx = Mathf.Clamp(playerx, minimumXY.x, maximumXY.x);
+				playery = Mathf.Clamp(playery, minimumXY.y, maximumXY.y);
+				transform.position = new Vector3(playerx, playery, transform.position.z);
+	}	
 }
