82a30c8a2550bc154cf355f05aa62b31275ac004
ArtofDrawingSwords <brandonscoolletters@hotmail.com>
2015-08-10 20:56:29 -0700

    movement and animation changes 1

---
+++ GameOfTheYear/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+    public SkeletonAnimation skeletonAnimation;
+
+    public string idleAnimation = "idle";
+    public string walkAnimation = "walk";
+    public string runAnimation = "run";
+    public string hitAnimation = "hit";
+    public string deathAnimation = "death";
+    public string jumpAnimation = "jump";
+    public string fallAnimation = "jump";
+    public string crouchAnimation = "idle";
+    public string currentAnimation  = "";
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.grounded)
+        {
+            {
+                if (x == 0)
+                    skeletonAnimation.AnimationName = idleAnimation;
+                else //Start walking into running
+                    skeletonAnimation.AnimationName = Mathf.Abs(x) > 0.6f ? runAnimation : walkAnimation;
+            }
+        }
+        else
+        {
+            if (velocity.y > 0)
+                skeletonAnimation.AnimationName = jumpAnimation;
+            else
+                skeletonAnimation.AnimationName = fallAnimation;
+        }
+    }
+
+    void SetAnimation(string anim, bool loop)
+    {
+        if (currentAnimation != anim)
+        {
+            skeletonAnimation.state.SetAnimation(0, anim, loop);
+            currentAnimation = anim;
+        }
+    }
+}
---
+++ GameOfTheYear/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private float xVelocity;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    public bool grounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundCheckLayerMask;
+
+    private bool doubleJumped;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundCheckLayerMask);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        xVelocity = Input.GetAxis("Horizontal");
+            
+        if(grounded)
+        {
+            doubleJumped = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            jump();
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && !doubleJumped && !grounded)
+        {
+            jump();
+            doubleJumped = true;
+        }
+        if(xVelocity > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if(xVelocity < 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	
+	}
+
+    private void jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+    }
+}
