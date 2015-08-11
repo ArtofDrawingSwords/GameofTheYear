094ad6bfec3ddddd01a28ad84978119e09fc845f
houseofkohina <bryankhamly@gmail.com>
2015-08-10 22:31:02 -0700

    Added TrollPlatform
    :3

---
+++ GameOfTheYear/Assets/Scripts/lolnope.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lolnope : MonoBehaviour {
+	public float delay = 0.5f;
+	private Rigidbody2D ground;
+	
+	void Awake()
+	{
+		ground = GetComponent<Rigidbody2D>();
+	}
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.CompareTag("spineboy"))
+		{
+			Invoke ("nope", delay);
+		}
+	}
+	void nope()
+	{
+		ground.isKinematic = false;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
