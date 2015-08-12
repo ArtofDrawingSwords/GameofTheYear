caf49ae3537b80e88103fe7c4fbefcac6deb8103
houseofkohina <bryankhamly@gmail.com>
2015-08-12 03:03:34 -0700

    Fixed Random Errors from previous commit.
    Unity dun goofed on me.

--- GameOfTheYear/Assets/Scripts/BlingPickup.cs
+++ GameOfTheYear/Assets/Scripts/BlingPickup.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class BlingPickup : MonoBehaviour {
 	private bool Blinged;//Got Em!
-	//public GameObject Sprite;
+	public GameObject Sprite;
 	public ParticleSystem Particles;
 	public SpriteRenderer SpriteRender;
-	private God Blingerino;
+
+	private God BlingMe;
 	// Use this for initialization
 	void Start () {
 	
@@ -20,18 +21,17 @@ public class BlingPickup : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.gameObject.tag == "spineboy" && Blinged == false) {
 			Blinged = true;
-			//Sprite.SetActive(false);
 			SpriteRender.enabled = false;
 			Particles.Emit(10);
 			Invoke ("DestroyMe", 0.5f);
-
-			if (Blingerino == null){
-				Blingerino = GameObject.FindGameObjectWithTag ("God").GetComponent<God>();
+			if(BlingMe == null)
+			{
+				BlingMe = GameObject.FindGameObjectWithTag ("God").GetComponent<God>();
 			}
-			Blingerino.Blingerino ();
+			BlingMe.Blingerino();
 		}
 	}
 	void DestroyMe(){
-		Destroy (this.gameObject );
+		Destroy (this.gameObject);
 	}
 }
